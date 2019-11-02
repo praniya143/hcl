57af6f2928fe8d23c3948878c4b6d426783bfc83
unknown <spksprabu@gmail.com>
2019-11-02 05:25:40 +0000

    Model & Controller

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,4 @@ Route::post('register', 'ApiController@register');
 Route::group(['middleware' => 'auth.jwt'], function () {
     Route::get('logout', 'ApiController@logout');
 
-    Route::get('user', function(){
-    });
-
-    Route::get('products', 'ProductController@index');
-    Route::get('products/{id}', 'ProductController@show');
-    Route::post('products', 'ProductController@store');
-    Route::put('products/{id}', 'ProductController@update');
-    Route::delete('products/{id}', 'ProductController@destroy');
 });
