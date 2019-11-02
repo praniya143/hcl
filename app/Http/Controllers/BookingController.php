2bd40b211eb66d33fae403f209644504cef83857
prabhu <prabhukannan.rg@gmail.com>
2019-11-02 09:35:13 +0000

    aas

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -17,18 +17,24 @@ class BookingController extends Controller
     }
 
     
-	public function history() {
-		
+	public function history(Request $request) {
+		$city = '';
+		if($request->input('city')){
+			$city = $request->input('city');
+		}
 		$history = \DB::table('booking')
             ->join('hotel_detail', 'booking.hotel_id', '=', 'hotel_detail.id')
+			->join('location', 'hotel_detail.location_id', '=', 'location.id')
             ->join('user_auth', 'user_auth.id', '=', 'booking.customer_id')
-            ->select('user_auth.name','hotel_detail.name As hotelname','booking_date','total_amount','no_of_persons','payment_type','check_in_date','check_out_date','city_name')
-			->where('booking.customer_id',1)
-            ->get();
+            ->select('user_auth.name','hotel_detail.name As hotelname','booking_date','total_amount','no_of_persons','payment_type','check_in_date','check_out_date','location.city_name as city_name','location')
+			->where('booking.customer_id',1);
+		if($city!='') {
+			$history->where('location.city_name','LIKE',"%$city%");
+		}
+        $res = $history->get()->toArray();
 
-		$res = $history->toArray();
-
-		return view("bookinghistory",compact('res'));
+		
+		return view("bookinghistory",compact('res','city'));
 
          
 	}	
